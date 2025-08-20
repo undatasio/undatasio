fc0fb05d42c4775358d2bb24ff6159863b0d6fe0
stay <staydxj9933@163.com>
2025-08-20 16:08:40 +0800

    undataio

---
+++ undatasio/setup.py
@@ -0,0 +1,48 @@
+from distutils.core import setup
+from setuptools import find_packages
+import pathlib
+import re
+import os
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+package = 'undatasio'
+
+
+def get_version():
+    """
+    从包的 __init__.py 文件中提取版本号.
+
+    参数:
+      package: 包的名称.
+
+    返回:
+      版本号字符串, 如果找不到则返回 '0.0.0'.
+    """
+    init_py = os.path.join(package, '__init__.py')
+    with open(init_py, 'r') as f:
+        content = f.read()
+
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", content, re.M)
+    if version_match:
+        return version_match.group(1)
+    else:
+        raise RuntimeError(f"无法在 '{init_py}' 中找到版本号")
+
+
+setup(name=package,
+      version=get_version(),
+      description='python sdk for the undatasio platform',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='undatasio',
+      author_email='alex.zhang@undatas.io',
+      url='',
+      install_requires=[
+          "requests~=2.32.3"
+      ],
+      license='MIT',
+      python_requires=">=3.10",
+      packages=find_packages()
+      )
